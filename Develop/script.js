a3f6bc8bb17940df693aa4f35962fade38c63aab
mitchmcnaughton <141999182+mitchmcnaughton@users.noreply.github.com>
2023-09-07 21:08:55 +1000

    added css comments

--- Develop/script.js
+++ Develop/script.js
@@ -1,35 +1,26 @@
-// Assignment Code
+// declaring variables
 var pass;
 var generateBtn = document.querySelector("#generate");
+var allowedPassword = ""
 
-// Write password to the #password input
+// function that once the password is generated writes it on screen
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
   passwordText.value = pass;
-
 }
 
   
-
+//a function to generate the password
 function generatePassword() {
   var passwordLength = prompt("How long do you want your password to be?(min 8, max 128)")
 
-
-  //checks if the password length is in between the requirements
-  
+  //checks if the password length is in between the requirements and if its a valid response.
   for (; passwordLength>128 ||passwordLength<8||isNaN(passwordLength);){
     passwordLength = prompt("Please enter a valid number between 8-128")
   }
 
-  console.log(parseInt(passwordLength));
-  
-
-  
-  
   //checks if the user wants to include lowercase letters,uppercase letters, numbers and special characters and makes sure they use y/n
-  
   var lowerCase = prompt("Do you want to include lowercase lettes? (y/n)")
   for (;lowerCase!=='y'&&lowerCase!=='n';){
     lowerCase = prompt("please enter y/n");
@@ -47,7 +38,7 @@ function generatePassword() {
     specialCharacter = prompt("please enter y/n");
   }
 
-
+  //checks for invalid inputs 
   for (;lowerCase==='n'&& upperCase==='n' &&numbers==='n'&&specialCharacter==='n';){
     var lowerCase = prompt("Please say yes to atleast 1 option! Do you want to include lowercase lettes? (y/n)")
     for (;lowerCase!=='y'&&lowerCase!=='n';){
@@ -65,54 +56,38 @@ function generatePassword() {
     for (;specialCharacter!=='y'&&specialCharacter!=='n';){
       specialCharacter = prompt("please enter y/n");
     }
-
-
   }
-  var allowedPassword = ""
 
+  //based on the users inputs adds the corrosponding sets of characters
   if (lowerCase=== 'y') {
     allowedPassword = allowedPassword+ "abcdefghijklmnopqrstuvwxyz";
   }
-
-  
 
   if (upperCase=== 'y') {
     allowedPassword = allowedPassword+ "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   }
 
-  
-
   if (numbers=== 'y') {
     allowedPassword = allowedPassword+ "0123456789";
   }
-
-  
 
   if (specialCharacter=== 'y') {
     allowedPassword = allowedPassword+ " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
   }
   
-  
-  
- 
+  //generates the password based on users requirements
   for (var i = 0; i <= passwordLength; i++) {
-    
-    
     var randomNumber = Math.floor(Math.random() *Math.floor(allowedPassword.length));
     pass += allowedPassword[randomNumber];
-    
   }
+  
+  //splices the password
   var length = passwordLength + 9;
   pass = pass.slice(9,length);
 
-  
-
- 
-
 }
 
-
-// Add event listener to generate button
+// adds the functionality when the user clicks the password is generated
 generateBtn.addEventListener("click", writePassword);
 
 
